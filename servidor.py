e8b584b280df285a78bb011ba71f77da26dedc19
Flavio Suarez <ps3flavio2024@gmail.com>
2026-07-11 17:06:56 -0300

    server version 1.1.6)

--- servidor.py
+++ servidor.py
@@ -199,11 +199,7 @@ async def websocket_endpoint(ws: WebSocket):
     try:
 
         raw = await ws.receive_text()
-        
-        print("PRIMER MENSAJE")
-        print(raw)
 
-       
         data = json.loads(raw)
 
         action = data.get("action")
@@ -373,9 +369,7 @@ async def websocket_endpoint(ws: WebSocket):
 
 
             raw = await ws.receive_text()
-            
-            print("MENSAJE DEL CHAT")
-            print(raw)
+
 
 
             try:
